74fb852303f809fbe52a1e75951bfd3811fb596c
«alexander <favorite@bk.ru>
2022-09-04 18:37:33 +0300

    hw3

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,42 +1,27 @@
-﻿int size = 4;
-int minValue = 100;
-int maxValue = 999;
-int[] array = new int[size];
-int x = 0;
+﻿//Разница между максимальным и минимальным элементами массива
+Console.Clear();
+int size = 6;
+double n = 0;
+Random rand = new Random();
+double[] array = new double[size];
+double max = 0;
+double min = 999;
+
 for (int i = 0; i < array.Length; ++i) {
-    array[i] = new Random().Next(minValue, maxValue);
+    
+    array[i] = rand.NextDouble()*100;
 }
-//Console.WriteLine(string.Join(",", array));
+Console.WriteLine(string.Join(", ", array));
 
-void PrintArray(int[] array)
+for (int i = 0; i < array.Length; i++) 
+
 {
-    int count = array.Length;
-
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
+ 
+if (array[i] <= min) min = array[i];
+if (array[i] > max) max = array[i];
+    n = max - min;
 }
-void SelectionSort(int[] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
-        int maxPosition = x;
+Console.WriteLine(Math.Round(min, 2) + " -> min");
+Console.WriteLine(Math.Round(max, 2) + " -> max");
 
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if(array[j] < array[minPosition]) minPosition = j;
-            if(array[j] > array[minPosition]) maxPosition = x;
-            Console.Write($"{array[i]} + {array[x]} ");
-        }
-            Console.Write($"{array[i]} + {array[x]} ");
-            //PrintArray(array);
-        //int temprorary = array[i];
-        //array[i] = array[minPosition];
-        //array[minPosition] = temprorary;
-    }
-}
-
-PrintArray(array);
+Console.WriteLine(Math.Round(n, 2) + " -> Разница между максимальным и минимальным элементами массива");
